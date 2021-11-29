4a933a4ba074e0148e56c3d6ffbbbd404ef4cd50
Kathryn Hurchla <kathryn@dadeda.design>
2021-11-29 11:31:55 -0500

    offline dash app work|finetuning dataset

---
+++ analysis/food_soil_data_analysis.py
@@ -0,0 +1,101 @@
+import xarray as xr
+# import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+# read the data file in with xarray and assign it to ds variable
+ds = xr.open_dataset("../data/SOCD5min.nc")
+# transform it to a dataframe assigned to df variable
+df = ds.to_dataframe()
+
+# try the subset to remove na values without going through the flattening of the index first
+# it works without resetting the index
+df2 = df[~df.SOCD.isna()]
+
+# flatten the index of the overall df2 dataframe (with NaN removed, before summary)
+df2flat = df2.reset_index()
+
+# for the dataframe with NA removed
+# translate lon and lat columns into a spatial geometry variable in a GeoPandas dataframe
+gdf2flat = gpd.GeoDataFrame(df2flat, geometry=gpd.points_from_xy(df2flat.lon, df2flat.lat))
+
+# Set a CRS on the geo dataframe object first
+# according to data source documentation, the coordinate system is WGS_1984
+# readme file is available at http://globalchange.bnu.edu.cn/download/doc/worldsoil/readme.zip
+gdf2flat.crs = "EPSG:4326"
+
+# take only the 4.5 depth records and
+# reset the index and drop the extra previous index column
+gdf2flatsurface = gdf2flat[gdf2flat['depth'] == 4.5].reset_index(drop=True)
+
+# commented out due to KeyError: "['index'] not found in axis"
+# # dropping unnecessary previous 'index' column
+# gdf2flatsurface = gdf2flatsurface.drop('index', axis=1)
+
+# use the world dataset from geopandas to get a link of points set 
+# to grab each soil measurement location's country from
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+# join my points geometry from gdf2flatsurface to the world and 
+# get the countries they are residing in using a spatial join (sjoin)
+result = gpd.sjoin(gdf2flatsurface, world, how='left').reset_index(drop=True)
+
+# drop NaN i.e. null values from the result of linking gdf2flatsurface to add country from world dataset
+# and drop the extra index column
+gdf2flatsurfacecountry = result.dropna().reset_index(drop=True)
+
+# rename name column to country_name, in place to replace column name in same column
+gdf2flatsurfacecountry.rename(columns={"name": "country_name", "pop_est": "country_pop_est", "iso_a3": "country_iso_a3", "gdp_md_est": "country_gdp_md_est"}, inplace=True)
+
+# load in the food trade detailed matrix copy freshly downloaded from https://www.fao.org/faostat/en/#data/TM to an alternate directory
+# adding , encoding = "ISO-8859-1" to resolve "UnicodeDecodeError: 'utf-8' codec can't decode byte 0xf4 in position 38698: invalid continuation byte"
+# alternately use the alias 'latin' for encoding
+dftrade_mx = pd.read_csv('/Users/kathrynhurchla/Documents/hack_mylfs_GitHub_projects/Trade_DetailedTradeMatrix_E_All_Data_(Normalized).csv', encoding = "ISO-8859-1")
+
+# filter for just the 'Export  Quantity' rows by its element code identified earlier
+dftrade_mx_xq = dftrade_mx[dftrade_mx['Element Code'] == 5910].reset_index(drop=True)
+
+# drop columns I do not need
+dftrade_mx_xq = dftrade_mx_xq.drop('Element Code', axis=1)
+dftrade_mx_xq = dftrade_mx_xq.drop('Year Code', axis=1)
+
+# for my web app I will remove the export quantity trade rows where year is not 2019,
+# i.e. I will keep only the most recent export dataset available
+# naming it to a new dataframe whilst resetting index and dropping the previous index
+dftrade_mx_xq2019 = dftrade_mx_xq.drop(dftrade_mx_xq.loc[dftrade_mx_xq['Year']!=2019].index, inplace=False).reset_index(drop=True)
+
+# read in the FAOSTAT key dataset as a variable
+faoSTATkey = pd.read_csv('/Users/kathrynhurchla/Documents/hack_mylfs_GitHub_projects/FAOSTAT_data_11-26-2021.csv')
+
+# For dftrade_mx_xq2019:
+# using pandas merge function, link the trade matrix reporter country code with key to append its ISO_3 code
+# with left data as food trade matrix
+dftrade_mx_xq2019ISO3 = pd.merge(left=dftrade_mx_xq2019, right=faoSTATkey[['Country Code','ISO3 Code']],
+                                     # key column from left dataframe
+                                     left_on='Reporter Country Code',
+                                     # key column from right dataframe
+                                     right_on='Country Code',
+                                     # merge as a 'left' join type, and 
+                                     # drop the duplicate key column used for join from right dataframe
+                                     how='left').drop('Country Code', 1)
+
+# There is no "China" in the trade matrix. I will fill China, mainland with China's ISO3 code 'CHN'
+# find rows with the Reporter Country Code 41 (for China, mainland), 
+# locate the 'ISO3 Code' column in those rows and set it to 'CHN'
+dftrade_mx_xq2019ISO3.loc[dftrade_mx_xq2019ISO3['Reporter Country Code'] == 41, 'ISO3 Code'] = 'CHN'
+
+# rename appended ISO3 column to clarify that it's for Reporter Country in trade matrix
+dftrade_mx_xq2019ISO3.rename(columns={"ISO3 Code": "Reporter Country ISO3"}, inplace=True)
+
+# using pandas merge function, link the trade matrix and socd dataframes
+# with left data as food trade
+# 'stopped here with output in Terminal 'zsh: killed     /Users/kathrynhurchla/opt/anaconda3/envs/envsoil/bin/python'
+dftrade_mx_xq2019_socdsurface = pd.merge(left=dftrade_mx_xq2019ISO3, right=gdf2flatsurfacecountry,
+                                     left_on='Reporter Country ISO3',
+                                     right_on='country_iso_a3', 
+                                     # merge as a 'left' join type, and 
+                                     # drop the duplicate key column used for join from right dataframe
+                                     how='left').drop('country_iso_a3', 1)
+
+# write a CSV of only the 4.5 depth socd merged with food trade data
+dftrade_mx_xq2019_socdsurface.to_csv('/Users/kathrynhurchla/Documents/hack_mylfs_GitHub_projects/dftrade_mx_xq2019_socdsurface.csv')
--- app.py
+++ app.py
@@ -2,23 +2,74 @@
 # run this app with 'python app.py' and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-# import config.py file for mapbox access token as config.access_token
+# ---------------------------------------------------------------------------------------- 
+# imports (boilerplate)
+# import config.py file containing my api requirement
 import config
-
+# access mapbox api requirement
 mapbox_token = config.access_token
 
+# import the required packages using their usual aliases
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+# return when online to uncomment/to run: conda install -n envsoil dash_bootstrap_components 
+# import dash_bootstrap_components as dbc
+from dash.dependencies import Output, Input
 import plotly.graph_objects as go # or plotly.express as px
 import pandas as pd
 
-app = dash.Dash(__name__)
+# create (instantiate) the app,
+# using the Bootstrap Flatly theme to align with my llc website in development
+# return when online to uncomment/to run: conda install -n envsoil dash_bootstrap_components 
+app = dash.Dash(__name__) #, external_stylesheets=[dbc.themes.FLATLY])
 
-# read the data from file of 4.5 depth soil organic carbon density (%)
-df = pd.read_csv('/Users/kathrynhurchla/Documents/hack_mylfs_GitHub_projects/gdf2flatsurface.csv')
+# -- read the food trade matrix data into pandas from CSV file of 2019 export quantities (exported from analysis in Jupyter Notebook)
+dffood = pd.read_csv('/Users/kathrynhurchla/Documents/hack_mylfs_GitHub_projects/dftrade_mx_xq2019ISO3.csv')
 
-df = df.head(50)
+# -- read the 4.5 depth soil organic carbon density (%) measurements to filter for selected food's trade export Reporter Countries (exported from analysis in Jupyter Notebook)
+dfsoil = pd.read_csv('/Users/kathrynhurchla/Documents/hack_mylfs_GitHub_projects/gdf2flatsurface.csv')
+
+# for a test limit the datapoints to first 50 records
+dffood = dffood.head(50)
+
+# ----------------------------------------------------------------------------------------
+# create the app's layout (a list of HTML and/or interactive components)
+app.layout = html.Div(children=[
+    html.H1(id='h1_text_heading', 
+        children='Sustain our Soil for our Food', style={'text-align': 'center'}
+    ),    
+    html.H3(id='h3_text_select_country', 
+        children='To begin, select a country where you generally eat.', style={'text-align': 'left'}
+    ),
+    html.H3(id='h3_text_select_food', 
+        children='Then, select a food you frequently eat.', style={'text-align': 'left'}
+    ),
+    html.P(id='p_text_explanatory_intro', 
+        children='Explore how much of the soil is made up of organic carbon where your favorite foods come from. Measurements are a density of organic carbon as a percentage of what makes up the soil at each location from the ground surface down to 4.5 centimeters deep.', 
+    style={'text-align': 'left'}
+    ),
+    html.Div([
+    # add a dropdown for audience member using app to select country where they generally eat
+    dcc.Dropdown(id='country_dropdown', 
+                 options=[{'label': country, 'value': country}
+                          for country in dffood['Partner Countries'].unique()]),
+    html.Br(),
+    html.Div(id='country_output'),
+    html.Div([
+    # add a dropdown for audience member using app to select a food they frequently eat
+    dcc.Dropdown(id='food_dropdown', 
+                 options=[{'label': food, 'value': food}
+                          for food in df['Item'].unique()]),
+    html.Br(),
+    html.Div(id='food_output')
+
+    # dcc.Graph(
+    #     id='map-socd',
+    #     figure=fig
+    # )
+])
+
 
 map_surface = go.Scattermapbox(
     name = 'SOCD Surface Depth',
@@ -53,21 +104,30 @@ fig = go.Figure(data=data, layout=layout) # or any Plotly Express function e.g. 
 
 # fig.update_layout(...)
 
-app.layout = html.Div(children=[
-    html.H1(
-        children='Sustain our Soil for our Food'
-    ),    
-
-    html.Div(children='Explore how much organic carbon is in the soil where your favorite foods come from.'
-    ),
-
-    # dcc.Graph(
-    #     id='map-socd',
-    #     figure=fig
-    # )
-])
-
 fig.show()
 
+# ----------------------------------------------------------------------------------------
+# callback functions
+
+# return the selected country from the dropdown menu
+@app.callback(Output('country_output', 'children'),
+              Input('country_dropdown', 'value'))
+# display to the audience their selection for confirmation
+def display_selected_country(country):
+    if country is None:
+        return '' # color = 'Nothing' if not also using this in other callbacks
+        return 'You selected ' + country
+
+# return the selected food from the dropdown menu
+@app.callback(Output('food_output', 'children'),
+              Input('food_dropdown', 'value'))
+# display to the audience their selection for confirmation
+def display_selected_food(food):
+    if food is None:
+        return '' # color = 'Nothing' if not also using this in other callbacks
+        return 'You selected ' + food        
+
+# ----------------------------------------------------------------------------------------
+# run the app
 if __name__ == '__main__':
     app.run_server(debug=True)  # if inside Jupyter Notebook, add use_reloader=False inside parens to turn off reloader
